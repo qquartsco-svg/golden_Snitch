117d85a6c572be7a358df08f2a04ad9230449290
GNJz <jazzin2official@gmail.com>
2026-03-30 12:47:00 +1100

    Revert "feat(v0.2.0): DCF Ω 건강도 브리지 + 버전 업"
    This reverts commit d7549dbc6f768fbf348df2d6ff33edd8e7eec717.

--- drone_robot_adapter/__init__.py
+++ drone_robot_adapter/__init__.py
@@ -10,7 +10,6 @@ from .ardupilot_bridge import build_ardupilot_command_envelope
 from .nexus_bridge import build_nexus_drone_signal, render_nexus_drone_lines
 from .px4_bridge import build_px4_command_envelope
 from .watchdog import BindingWatchdog
-from .health_bridge import dcf_omega_or_fallback, omega_verdict
 
 __all__ = [
     "ArduPilotCommandEnvelope",
@@ -22,8 +21,6 @@ __all__ = [
     "build_nexus_drone_signal",
     "build_px4_command_envelope",
     "render_nexus_drone_lines",
-    "dcf_omega_or_fallback",
-    "omega_verdict",
 ]
 
-__version__ = "0.2.0"
+__version__ = "0.1.2"
